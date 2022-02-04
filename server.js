da672887f296ca60544f729d3e03cfccd828bcac
Daffa Arviano <daffa.arviano@outlook.com>
2022-02-04 20:27:56 +0700

    changed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ fastify.register(require('./routes/watchlist_routes'), { prefix: '/watchlist' })
 
 const start = async () => {
     try {
-        await fastify.listen(3000, '0.0.0.0')
+        await fastify.listen(process.env.PORT || 3000, '0.0.0.0')
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
